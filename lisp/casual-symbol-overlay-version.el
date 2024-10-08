9481daf1b9b027dbd12cfed8219b89e73ec3728a
Charles Choi <kickingvegas@gmail.com>
2024-10-08 16:17:47 -0700

    Bump version to 1.1.0

--- lisp/casual-symbol-overlay-version.el
+++ lisp/casual-symbol-overlay-version.el
@@ -22,7 +22,7 @@
 
 ;;; Code:
 
-(defconst casual-symbol-overlay-version "1.0.3-rc.1"
+(defconst casual-symbol-overlay-version "1.1.0"
   "Casual Symbol Overlay Version.")
 
 (defun casual-symbol-overlay-version ()
